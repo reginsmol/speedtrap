fcc2a12536abe02ddfe92e1db3da01e3b897dbe0
Matt <contact@hiddenlayer.dev>
2025-01-28 17:04:50 -0600

    add ALPR api call

--- api/Sources/App/Controllers/CaptureController.swift
+++ api/Sources/App/Controllers/CaptureController.swift
@@ -28,6 +28,23 @@ struct CaptureController: RouteCollection {
         let capture = try req.content.decode(CaptureDTO.self).toModel()
 
         try await capture.save(on: req.db)
+        let token = Environment.get("ALPR_TOKEN")!
+
+        let response = try await req.client.post("https://api.platerecognizer.com/v1/plate-reader")
+        { req in
+
+            let imageUrl =
+                "https://loadql.dev/cdn-cgi/imagedelivery/QcwQm0dWPprrgKR8Ke1MXw/\(capture.fileId)/public"
+            // Encode JSON to the request body.
+
+            try req.content.encode(["upload_url": imageUrl])
+
+            // Add auth header to the request
+            req.headers.add(name: "Authorization", value: "Token \(token)")
+        }
+
+        print(response)
+
         return capture.toDTO()
     }
 }
